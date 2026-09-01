342261feecd6c683b3fb98aed2ca3e360c425a93
yuk1ss <113019276@qq.com>
2026-09-01 19:09:12 +0800

    chore: add project ignore rules

---
+++ Algorithm/Hello.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    string str;
+    str = "";
+    cout << str.size() << endl;
+    return 0;
+}
---
+++ Algorithm/string.md
@@ -0,0 +1,4 @@
+`@.size()` 返回字符串大小
+`@.empty()` 判断是否为空
+`@.substr(a,b)` 截取字符串
+`to_string(int)` 转换成字符串
